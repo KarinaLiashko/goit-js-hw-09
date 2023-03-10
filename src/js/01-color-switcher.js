bce4bdc7b5d5052bb8a9f670587f1760c7325b15
KarinaLiashko <112810602+KarinaLiashko@users.noreply.github.com>
2023-03-10 20:32:04 +0100

    01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+let intervalValue = null; 
 
+document.querySelector("[data-start]").addEventListener("click", function () { 
+
+    intervalValue = setInterval(function getRandomHexColor() {
+         const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+   
+   document.body.style.backgroundColor = randomColor;
+    }, 1000);
+   });
+document.querySelector("[data-stop]").addEventListener("click", function () {
+    clearInterval(intervalValue);
+  });
